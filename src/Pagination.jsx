be3b8d09f28bd4fa1a3f52c2d2097344da6e9a10
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-06 20:46:11 +0530

    completed

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 const Pagination = () => {
-  return (
-    <div>Pagination</div>
-  )
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const rowsPerPage = 10;
+  
+    useEffect(() => {
+      fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch(() => setError('Failed to fetch data'));
+    }, []);
+  
+    const indexOfLastRow = currentPage * rowsPerPage;
+    const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+    const currentRows = data.slice(indexOfFirstRow, indexOfLastRow);
+  
+    const totalPages = Math.ceil(data.length / rowsPerPage);
+  
+    const handleNextPage = () => {
+      if (currentPage < totalPages) {
+        setCurrentPage(currentPage + 1);
+      }
+    };
+  
+    const handlePreviousPage = () => {
+      if (currentPage > 1) {
+        setCurrentPage(currentPage - 1);
+      }
+    };
+  
+    if (error) {
+      return <div>{error}</div>;
+    }
+  
+    if (data.length === 0) {
+      return <div>Loading...</div>;
+    }
+  
+    return (
+      <div>
+        <h1 style={{textAlign: 'center'}}>Employee Data Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentRows.map((employee) => (
+              <tr key={employee.id}>
+                <td>{employee.id}</td>
+                <td>{employee.name}</td>
+                <td>{employee.email}</td>
+                <td>{employee.role}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className='buttons'>
+          <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+            Previous
+          </button>
+          <span className="page-number"> {currentPage} </span>
+          <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+            Next
+          </button>
+        </div>
+      </div>
+    );
 }
 
 export default Pagination
